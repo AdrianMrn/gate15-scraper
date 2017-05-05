bbae5b21a2f8b0d8d0cce6a03d46134f68ec7edc
adriaanmarain <adriaanmarain300@gmail.com>
2017-05-05 17:23:15 +0200

    sending articles to mysql

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 var rp = require('request-promise');
 var async = require('async');
+var mysql = require('mysql');
+var env = require('./env');
+
+
+var mysqlCredentials = env.mysqlCredentials();
+var connection = mysql.createConnection({
+  host     : mysqlCredentials.host,
+  user     : mysqlCredentials.user,
+  password : mysqlCredentials.password,
+  database : mysqlCredentials.database
+});
 
 function scrapePage(page) {
     rp(page)
@@ -7,47 +18,62 @@ function scrapePage(page) {
         pageData = JSON.parse(pageData);
         news = pageData.data;
 
+        connection.connect();
+
+        var articleNumber = 1;
         async.each(news, function(newsItem, callbackArticle) {
             var article = {};
             var title, articleUrl, author, imageUrl, publishedAt;
-            var articleContent = [];
+            var articleContent = {};
 
             title = newsItem.title;
             articleUrl = "https://www.gate15.be" + newsItem.uriPrefix + "/" + newsItem.slug;
             author = "gate15";
             publishedAt = newsItem.publishedAt;
             
+            var snippetPart = 1;
             async.each(newsItem.snippets, function(snippet, callbackSnippet) {
                 //only first type=media is the article pic
                 if (snippet.type == "media" && !imageUrl) {
                     imageUrl = snippet.body.file[0].src;
                 } else {
-                    articleContent.push(snippet.body.text);
+                    //articleContent.push(snippet.body.text);
+                    articleContent[snippetPart] = snippet.body.text;
+                    snippetPart++;
                 }
                 
                 callbackSnippet();
             }, function() {
-                /*console.log("title:",title);
-                console.log("author:",author);
-                console.log("articleUrl:",articleUrl);
-                console.log("imageUrl:",imageUrl);
-                console.log("publishedAt:",publishedAt);
-                console.log("articleContent:",articleContent);*/
                 
                 article.title = title;
                 article.author = author;
-                article.articleUrl = articleUrl;
-                article.imageUrl = imageUrl;
-                article.publishedAt = publishedAt;
-                article.articleContent = articleContent;
+                article.article_url = articleUrl;
+                article.picture_url = imageUrl;
+                article.published_on = publishedAt.substring(0,10);
+                article.content = JSON.stringify(articleContent);
+                article.is_accepted = 0;
 
-                console.log(article);
+                console.log(articleNumber);
+                articleNumber++;
 
-                console.log("");
-                callbackArticle();
+                connection.query('SELECT id FROM gate15_articles WHERE title="' + title + '"', function (error, result, fields) {
+                    //if (error) throw error;
+                    if (result[0]) {
+                        //console.log("article already exists, doing nothing");
+                        callbackArticle();
+                    } else {
+                        var query = connection.query('INSERT INTO gate15_articles SET ?', article, function(err, result) {
+                        if (err) console.log(err);
+                            //console.log('result:', result);
+                        });
+                        //console.log(query.sql);
+                        callbackArticle();
+                    }
+                });
             });
         }, function() {
             console.log("Finished getting articles.");
+            connection.end();
         });
 
     })
@@ -57,4 +83,4 @@ function scrapePage(page) {
 
 }
 
-scrapePage("https://www.gate15.be/srv/content/d/content-type/10/start/0/limit/10/excluded_tags/trots");
+scrapePage("https://www.gate15.be/srv/content/d/content-type/10/start/0/limit/50/excluded_tags/trots");
---
+++ env.js.ex
@@ -0,0 +1,8 @@
+//mysql info goes here
+exports.mysqlCredentials = function() {
+    var host = 'localhost'
+    var user = 'user'
+    var password = 'password'
+    var database = 'database'
+    return {host,user,password,database};
+}
